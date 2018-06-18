391cb680012e158cc79b92d2b6869df68d769828
HuuDV <huu.dv@glv.co.jp>
2018-06-18 19:21:44 +0900

    get post item

--- BlogScrapy/items.py
+++ BlogScrapy/items.py
@@ -9,6 +9,5 @@ import scrapy
 
 
 class BlogscrapyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    links = scrapy.Field()
--- BlogScrapy/spiders/myblog.py
+++ BlogScrapy/spiders/myblog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from bs4 import BeautifulSoup
-
-
+from urllib import parse
+from BlogScrapy.items import BlogscrapyItem
 
 class MyblogSpider(scrapy.Spider):
     name = 'myblog'
@@ -11,6 +11,20 @@ class MyblogSpider(scrapy.Spider):
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, "lxml")
+
+        # 各ブログポストの <div> を取得する
+        posts = soup.find_all('div', {'class': 'title'})
+        for post in posts:
+            post_url = post.a['href']
+            # URL から記事の ID を取得する
+            parse_result = parse.urlparse(post_url)
+            path_segments = parse_result.path.split('/')
+            post_id = path_segments[2]
+            # 記事の ID を指定して個別のポストを処理する Spider を生成する
+            spider = MyblogPostSpider(post_id)
+            # リクエストを処理するコールバックに上記の Spider のメソッドを指定する
+            req = scrapy.Request(url=post_url, callback=spider.parse)
+            yield req
 
         # 次のページへのリンクが入った <li> を取得する
         next_page = soup.find('li', {'class': 'next'})
@@ -25,8 +39,40 @@ class MyblogSpider(scrapy.Spider):
         baseurl = 'http://momijiame.tumblr.com'
         path = next_page_link['href']
         url = '{baseurl}{path}'.format(baseurl=baseurl, path=path)
-        print(url)
 
         # scrapy.Request を返すと次にクロールするページの指定になる
         next_crawl_page = scrapy.Request(url)
         yield next_crawl_page
+
+
+
+
+class MyblogPostSpider(scrapy.Spider):
+    name = 'myblogpost'
+    allowed_domains = ['momijiame.tumblr.com']
+
+    def __init__(self, post_id, *args, **kwargs):
+        super(MyblogPostSpider, self).__init__(*args, **kwargs)
+
+        self.post_id = post_id
+        url = 'http://momijiame.tumblr.com/post/{post_id}'.format(
+            post_id=self.post_id,
+        )
+        self.start_urls = [url]
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, "lxml")
+
+        content = soup.find('div', {'class': 'body'})
+
+        links = content.find_all('a')
+        if len(links) < 1:
+            # 記事にハイパーリンクが含まれていないので終了
+            return
+
+        # ハイパーリンクがあったらタイトルと共にアイテムを返す
+        title_div = soup.find('div', {'class': 'title'})
+        title = title_div.a.text
+        link_urls = [link['href'] for link in links]
+        item = BlogscrapyItem(title=title, links=link_urls)
+        return item
